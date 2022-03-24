efa0238fc525c9b58299fc8b7c5ca538f90f24ad
Dorilyn Loring <dorilyn.loring@gmail.com>
2022-03-23 23:26:57 -0600

    started the starships folder

---
+++ starships/sw-starships.js
@@ -0,0 +1,13 @@
+import { starships } from '../data/starships.js'
+
+const nav = document.querySelector('.nav')
+const navList = document.querySelector('navList')
+const shipView = document.querySelector('shipViewer')
+
+function populateNav() {
+
+}
+
+function populateShipView() {
+    
+}
