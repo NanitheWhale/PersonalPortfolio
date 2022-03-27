6209479687983f0fcadf8ed325c9621f5ea5dd06
Dorilyn Loring <dorilyn.loring@gmail.com>
2022-03-26 21:31:07 -0600

    inserted the starship photos

--- starships/sw-starships.js
+++ starships/sw-starships.js
@@ -1,13 +1,37 @@
 import { starships } from '../data/starships.js'
+import { removeChildren, getLastNumber } from '../utils/index.js'
 
 const nav = document.querySelector('.nav')
-const navList = document.querySelector('navList')
-const shipView = document.querySelector('shipViewer')
+const navList = document.querySelector('.navList')
+const shipViewer = document.querySelector('.shipViewer')
 
 function populateNav() {
+starships.forEach((starship) => {
+    let anchor = document.createElement('a')
+    anchor.href = '#'
+    anchor.textContent = starship.name
+    const listItem = document.createElement('li')
 
+
+    anchor.addEventListener('click', () => populateShipView(starship))
+
+    listItem.appendChild(anchor)
+    navList.appendChild(listItem)
+    })
 }
 
-function populateShipView() {
-    
+populateNav()
+
+function populateShipView(shipData) {
+    removeChildren(shipViewer)
+    const shipImage = document.createElement('img')
+    let shipNum = getLastNumber(shipData.url)
+    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+
+    shipImage.addEventListener('error', () => {
+    console.log('We got an error!')
+    shipImage.hidden = true
+    })
+
+    shipViewer.appendChild(shipImage)
 }
