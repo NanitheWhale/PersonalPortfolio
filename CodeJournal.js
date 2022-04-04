80765d2a9fd1e7d79c583902a3377a41aa2c2da8
Dorilyn Loring <dorilyn.loring@gmail.com>
2022-04-04 15:48:15 -0600

    added the senators

--- CodeJournal.js
+++ CodeJournal.js
@@ -135,4 +135,31 @@ const pilotNames = pilots.map(pilot => pilot.name) // new array just contains pi
 
 // Ternary operator system: condition 7 exprIfTrue : exprIfFalse
 
- 
+ const swpilots = [
+     {
+         id: 10,
+         name: "Poe Dameron",
+         years: 14
+     },
+     {
+         id: 2,
+         name: "Temmin 'Snap' Wexley",
+         years: 30
+     },
+     {
+         id: 41,
+         name: "Tallissan Lintra",
+         years: 16,
+     },
+     {
+         id: 99,
+         name: "Ello Asty",
+         years: 22
+     },
+ ];
+
+ const totalYears = swpilots.reduce((acc, pilot) => acc + pilot.years, 0)
+
+ const mostExpPilot = swpilots.reduce((oldest, pilot) => {
+     return (oldest.years || 0) > pilot.years ? oldest : pilot 
+ }, {})
--- congress/congress.js
+++ congress/congress.js
@@ -1,16 +1,18 @@
-import {senators} from '../data/senators.js'
+import { senators } from '../data/senators.js'
 
-console.log(senators)
+const senatorDiv = document.querySelector('.senatorsDiv')
+const seniorityHeading = document.querySelector('.seniority')
+const loyaltyList = document.querySelector('.loyaltyList')
 
 function simplifiedSenators() {
     return senators.map(senator => {
-        const middleName = senator.middle_name ? `${senator.middle_name}` : ` `
+        const middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
         return {
             id: senator.id,
             name: `${senator.first_name}${middleName}${senator.last_name}`,
             party: senator.party,
             gender: senator.gender,
-            imgURL: `https://www.govtrack.us/static/legislater-photos/${senator.govtrack_id}-100px.jpeg`,
+            imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`,
             seniority: +senator.seniority,
             missedVotesPct: senator.missed_votes_pct,
             loyaltyPct: senator.votes_with_party_pct
@@ -18,9 +20,36 @@ function simplifiedSenators() {
     })
 }
 
-function populateSenatorDiv(simpleSenators) {
-    // simpleSenators.forEach..
-    // create figure and image and figcaption elements
-    //set the image src to the senator's imgURL
-    // append them to the DOM
+const simpleSenators = simplifiedSenators()
+
+function populateSenatorDiv(senatorArray) {
+    senatorArray.forEach(senator => {
+        const senFigure = document.createElement('figure')
+        const figImg = document.createElement('img')
+        const figCaption = document.createElement('figCaption')
+    
+
+    figImg.src = senator.imgURL
+    figCaption.textContent = senator.name
+
+    senFigure.appendChild(figImg)
+    senFigure.appendChild(figCaption)
+    senatorDiv.appendChild(senFigure)
+})
 }
+
+populateSenatorDiv(simpleSenators)
+
+const mostSeniorMember = simplifiedSenators().reduce((acc, senator) => {
+    return acc.seniority > senator.seniority ? acc : senator  
+})
+
+seniorityHeading.textContent = `The most senior member of the senate is ${mostSeniorMember.name}`
+
+simplifiedSenators().forEach(senator => {
+    if(senator.loyaltyPct === 100) {
+        let listItem = document.createElement('li')
+        listItem.textContent = senator.name
+        loyaltyList.appendChild(listItem)
+    }
+})
