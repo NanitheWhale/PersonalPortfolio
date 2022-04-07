2521ea349b909112cae50510c0b09e5896ee19a8
NanitheWhale <dorilyn.marie555@gmail.com>
2022-04-07 12:03:53 -0600

    added snorlax image

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,17 +1,52 @@
-
-
-function getAPIData(url) {
+const getAPIData = async (url) => {
     try {
-        return fetch(url).then((data) => data.json())
+        const result = await fetch(url)
+        return await result.json()
     } catch (error) {
         console.error(error)
     }
 }
 
-function loadPokemon() {
-getAPIData(`https://pokeapi.co/api/v2/pokemon/snorlax`).then(
-    (data => console.log(data))
-)
+async function loadPokemon() {
+    const pokeData = await getAPIData(`https://pokeapi.co/api/v2/pokemon/snorlax`) 
+populatePokeGrid(pokeData)
+}
+
+
+const pokeGrid = document.querySelector('.pokeGrid')
+
+function populatePokeGrid(pokemonArray) {
+    // loop through all the pokemon and create individual pokecards
+    populatePokeCard(pokemonArray[0])
+}
+
+function populatePokeCard(pokemon) {
+    const pokeScene = document.createElement('div')
+    pokeScene.className = 'scene'
+    const pokeCard = document.createElement('div')
+    pokeCard.className = 'card'
+    pokeCard.addEventListener('click', () => pokeCard.classList.toggle('is-flipped'))
+
+    pokeCard.appendChild(populateCardFront(pokemon))
+    pokeScene.appendChild(pokeCard)
+    pokeGrid.appendChild(pokeScene)
+}
+
+function populateCardFront(pokemon) {
+const pokeFront = document.createElement('figure')
+pokeFront.className = 'cardFace'
+const pokeImg = document.createElement('img')
+pokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/143.png`
+const pokeCaption = document.createElement('figcaption')
+pokeCaption.textContent = 'snorlax'
+
+pokeFront.appendChild(pokeImg)
+pokeFront.appendChild(pokeCaption)
+return pokeFront 
+}
+
+function populateCardBack(pokemon) {
+
 }
 
 loadPokemon()
